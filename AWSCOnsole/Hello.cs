bdc3498bac1d76f7b42a93146744a13691a82d0e
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-14 19:03:28 +0530

    initialcommit\

---
+++ AWSCOnsole/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSCOnsole
+{
+  public class Hello
+    {
+        public string GetName()
+        {
+            string name = "My Pipeline Successful";
+            Console.WriteLine(name); 
+            return name;    
+        }
+       
+}
+}
---
+++ AWSCOnsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSCOnsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            Hello hello = new Hello();
+            hello.GetName();
+
+        }
+    }
+}
