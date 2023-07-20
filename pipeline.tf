f2709c317152f3ca0600eb20f11665241bb5bed2
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-20 14:33:49 +0530

    new pipeline

--- pipeline.tf
+++ pipeline.tf
@@ -117,3 +117,142 @@ resource "aws_codepipeline" "cicd_pipeline" {
 
 
 }
+resource "aws_codebuild_project" "tf-plan-console" {
+  name          = "tf-cicd-plan2"
+  description   = "Plan stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:1.4.6"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec.yml")
+ }
+}
+
+
+
+resource "aws_codepipeline" "cicd_pipeline_tf" {
+
+    name = "tf-cicd"
+    role_arn = aws_iam_role.tf-codepipeline-role.arn
+
+    artifact_store {
+        type="S3"
+        location = aws_s3_bucket.codepipeline_artifacts.id
+    }
+
+    stage {
+        name = "Source"
+        action{
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeStarSourceConnection"
+            version = "1"
+            output_artifacts = ["tf-code"]
+            configuration = {
+                FullRepositoryId = "PradeepaLakshmanan-CH-2022/TerraformRepo"
+                BranchName   = "master"
+                ConnectionArn = var.codestar_connector_credentials
+                OutputArtifactFormat = "CODE_ZIP"
+            }
+        }
+    }
+
+    stage {
+        name ="Plan"
+        action{
+            name = "Build"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-cicd-plan2"
+            }
+        }
+    }
+}
+resource "aws_codepipeline" "cicd_pipeline" {
+
+    name = "NewGitPipelineConsoleApplication"
+    role_arn = aws_iam_role.tf-codepipeline-role.arn
+
+    artifact_store {
+        type="S3"
+        location = aws_s3_bucket.codepipeline_artifacts.id
+
+    }
+
+   stage {
+    name = "Source"
+
+    action {
+      name            = "Source"
+      category        = "Source"
+      owner           = "AWS"
+      provider        = "CodeStarSourceConnection"
+      version         = "1"
+            output_artifacts = ["SourceArtifact"]
+            configuration = {
+                FullRepositoryId = "PradeepaLakshmanan-CH-2022/AWsConsoleApplication"
+                BranchName   = "main"
+                ConnectionArn=var.codestar_connector_credentials
+                OutputArtifactFormat = "CODE_ZIP"
+            }
+        }
+    }
+
+    stage {
+        name ="Build"
+        action{
+            name = "BuildAction"
+            category = "Build"
+            owner = "AWS"
+            provider = "CodeBuild"
+            version = "1"          
+            input_artifacts = ["SourceArtifact"]
+            output_artifacts = ["BuildArtifact"]
+            configuration = {
+                ProjectName = "NewConsoleProjectBuild"
+            }
+        }
+    }
+
+ stage {
+  name = "Deploy"
+
+  action {
+    name            = "DeployEC2"
+    category        = "Deploy"
+    owner           = "AWS"
+    provider        = "CodeDeploy"
+    version = "1"
+    run_order       = 1
+    input_artifacts = ["BuildArtifact"]
+
+    configuration = {
+      ApplicationName  = "NewConsoleAppDeployment"
+      DeploymentGroupName = "NewConsoleAppDeploymentGroup"
+  
+    }
+  }
+}
+
+
+}
+
