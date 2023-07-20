9665faced7885133dfc6b6a5776f133ad91331ba
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-20 15:27:35 +0530

    buildspec

--- pipeline.tf
+++ pipeline.tf
@@ -142,30 +142,6 @@ resource "aws_codebuild_project" "tf-plan-console" {
  }
 }
 
-resource "aws_codebuild_project" "tf-apply" {
-  name          = "tf-cicd-apply"
-  description   = "Apply stage for terraform"
-  service_role  = aws_iam_role.tf-codebuild-role.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:0.14.3"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "SERVICE_ROLE"
-    registry_credential{
-        credential = var.dockerhub_credentials
-        credential_provider = "SECRETS_MANAGER"
-    }
- }
- source {
-     type   = "CODEPIPELINE"
-     buildspec = file("appspec.yml")
- }
-}
 
 resource "aws_codepipeline" "cicd_pipeline_tf" {
 
@@ -209,19 +185,6 @@ resource "aws_codepipeline" "cicd_pipeline_tf" {
             }
         }
     }
-      stage {
-        name ="Deploy"
-        action{
-            name = "Deploy"
-            category = "Build"
-            provider = "CodeBuild"
-            version = "1"
-            owner = "AWS"
-            input_artifacts = ["tf-code"]
-            configuration = {
-                ProjectName = "tf-cicd-apply"
-            }
-        }
-    }
+ 
 
 }
