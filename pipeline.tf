09fd519ba867667f78eb6c4c2d187a8a9b238f58
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-20 16:34:10 +0530

    build added

--- pipeline.tf
+++ pipeline.tf
@@ -20,6 +20,28 @@ resource "aws_codebuild_project" "tf-plan" {
 
  
 }
+resource "aws_codebuild_project" "tf-plan-dotnet" {
+  name          = "ConsoleBuild"
+  description   = "stage for Terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type            = "LINUX_CONTAINER"
+  }
+
+  source {
+    type   = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+
+ 
+}
 
 resource "aws_codedeploy_app" "code_deploy" {
   name          = "NewConsoleAppDeployment"
